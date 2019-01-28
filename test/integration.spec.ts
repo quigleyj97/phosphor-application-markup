69ba0b68abc44b9b040813c46ddf4eac922aefb3
Joe Quigley <quigley.joseph@outlook.com>
2019-01-28 00:56:04 -0500

    Add: Integration test using hello world markup example
    This creates a box panel that has a label and 2 images (one kitten, one puppy)

---
+++ test/integration.spec.ts
@@ -0,0 +1,19 @@
+import { readFileSync } from "fs";
+import { MarkupLoader } from "../src";
+import { MessageLoop } from "@phosphor/messaging";
+
+describe("Integration test", () => {
+    let file: string;
+
+    beforeAll(() => {
+        file = readFileSync("./examples/testdoc.xml", {encoding: "utf8"});
+    })
+
+    test("Should render the HelloWorld example", () => {
+        let loader = new MarkupLoader();
+        const out = loader.loadXml(file);
+        MessageLoop.flush();
+        console.log((out as any));
+        expect(out.node.outerHTML).toMatchSnapshot();
+    })
+})
